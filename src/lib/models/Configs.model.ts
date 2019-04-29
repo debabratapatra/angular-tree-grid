286780ef367490a02ba71eb232920b5656b4b511
debabratapatra <debabratapatra12@gmail.com>
2019-04-29 10:36:39 +0530

    Implement conditional row edit and delete.

--- src/lib/models/Configs.model.ts
+++ src/lib/models/Configs.model.ts
@@ -6,11 +6,11 @@ export interface Configs {
     css: CssClass;
     columns?: Column[];
     data_loading_text: string;
-    row_class_function: Function;
     parent_id_field?: string;
     parent_display_field?: string;
     id_field?: string;
     actions: Actions;
-    row_edit_function: any;
-    row_delete_function: any;
+    row_class_function: Function;
+    row_edit_function: Function;
+    row_delete_function: Function;
 }
